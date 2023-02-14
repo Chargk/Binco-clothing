e9325404dc961b1b02ecf4ef01fda9fba102cbd6
Dima <mthwhsn@gmail.com>
2023-02-14 23:49:47 +0200

    adding a footer as well as fontawesome kit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Footer from "./components/footer/footer";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -10,13 +11,16 @@ const Shop = () => {
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-      </Route>
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
